0078ee54f1dc353cfd2671f3121fdacaab00aa8e
owlur <8864248@naver.com>
2017-09-01 13:30:45 +0900

    푸쉬서버!

--- push_server.py
+++ push_server.py
@@ -1,18 +1,37 @@
 import requests
-from urllib.parse import urlencode
 import xmltodict
 import time
+import sys
+import os
+import django
+from urllib.parse import urlencode
+from pyfcm import FCMNotification
 
-def send_fcm(title):
-    print(title)
+sys.path.append('D:\shelter_run')
+os.environ['DJANGO_SETTINGS_MODULE'] = 'shelter_run.settings'
+django.setup()
+from app_receiver.models import User
+
+def send_fcm(title, key):
+    push_service = FCMNotification(api_key=key)
+    registration_ids = [str(i) for i in User.objects.all() if i is not None]
+
+    message_title = title
+    message_body = "쉘터런을 켜서 가까운 대피소로 달리세요!"
+    result = push_service.notify_multiple_devices(registration_ids=registration_ids, message_title=message_title,
+                                                  message_body=message_body)
+
+    print(result)
 
 if __name__ == '__main__':
     url = 'http://newsky2.kma.go.kr/service/WetherSpcnwsInfoService/WeatherWarningList?'
     f= open('keys.txt')
     param = {
-        'ServiceKey': f.readline(),
+        'ServiceKey': f.readline()[:-1],
         'stnId': '143',
         'fromTmFc': '20170101'}
+
+    firebase_api_key = f.readline()
 
     pre = [{'tmSeq': '0', 'title': 'FIRST'}]
 
@@ -23,8 +42,10 @@ if __name__ == '__main__':
         current = xmltodict.parse(r.text)['response']['body']['items']['item']
         for i in current:
             print(i)
-            if int(i['tmSeq']) > int(pre[0]['tmSeq']):# and '풍랑' in i['title'] and '발표' in i['title']:
-                send_fcm(i['title'])
+            if int(i['tmSeq']) > int(pre[0]['tmSeq']) and '발표' in i['title']:
+                send_fcm(i['title'].split(' / ')[1], firebase_api_key)
+            else:
+                break
         pre = current
         print()
 
