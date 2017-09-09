1abeeecc76c898d954bb77adce210b3a3be235da
owlur <8864248@naver.com>
2017-09-09 20:03:42 +0900

    푸쉬서버!

--- push_server.py
+++ push_server.py
@@ -12,20 +12,23 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'shelter_run.settings'
 django.setup()
 from app_receiver.models import User
 
+
 def send_fcm(title, key):
     push_service = FCMNotification(api_key=key)
     registration_ids = [str(i) for i in User.objects.all() if i is not None]
 
     message_title = title
-    message_body = "쉘터런을 켜서 가까운 대피소로 달리세요!"
+    message_body = "달리자대피소를 켜서 가까운 대피소로 달리세요!"
     result = push_service.notify_multiple_devices(registration_ids=registration_ids, message_title=message_title,
                                                   message_body=message_body)
 
     print(result)
 
 if __name__ == '__main__':
+
     url = 'http://newsky2.kma.go.kr/service/WetherSpcnwsInfoService/WeatherWarningList?'
-    f= open('keys.txt')
+    f = open('keys.txt')
+
     param = {
         'ServiceKey': f.readline()[:-1],
         'stnId': '143',
@@ -40,12 +43,14 @@ if __name__ == '__main__':
         query_params = urlencode(param, safe='%')
         r = requests.get(url + query_params)
         current = xmltodict.parse(r.text)['response']['body']['items']['item']
-        for i in current:
-            print(i)
-            if int(i['tmSeq']) > int(pre[0]['tmSeq']) and '발표' in i['title']:
-                send_fcm(i['title'].split(' / ')[1], firebase_api_key)
+
+        for emergency in current:
+            print(emergency)
+            if int(emergency['tmSeq']) > int(pre[0]['tmSeq']) and '발표' in emergency['title']:
+                send_fcm(emergency['title'].split(' / ')[1].split('(')[0], firebase_api_key)
             else:
                 break
+
         pre = current
         print()
 
